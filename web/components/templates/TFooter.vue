234c960cbf3b6e9ae418dae940b34a6b803525f1
Rafaela <rafaelars.bh@gmail.com>
2023-08-04 11:21:38 -0300

    Version

--- web/components/templates/TFooter.vue
+++ web/components/templates/TFooter.vue
@@ -14,7 +14,7 @@ export default {
     return {
       fixed: false,
       companyName: 'Exato Consultoria',
-      appVersion: '1.0',
+      appVersion: '1.0.2',
       currentYear: new Date().getFullYear(),
     }
   },
